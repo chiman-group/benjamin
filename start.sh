cf60bc3b93f497e49f0aacdbf969978280a5ddbf
Samim Pezeshki <p.samim@gmail.com>
2015-11-28 02:02:30 +0330

    Add startup scripts

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+export TELEGRAM_TOKEN="170538059:AAFDDx3Ct-O1WgfCp2SlQkd3l3HAtIhYuo8"
+export TELEGRAM_INTERVAL=500
+export TELEGRAM_PRIVACY=0
+exec ./bin/hubot -a telegram -n ASOB_Bot -l ASOB_Bot
